a4eee271650ced017aa6ba5eb2af178c0ccf6487
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-07-26 00:47:26 +0200

    Command line tool added

--- makeitfair/fairrun.py
+++ makeitfair/fairrun.py
@@ -1,4 +1,16 @@
 from fairlib import *
-doi = "http://dx.doi.org/10.17632/nhtjgdkft4.1"
+import sys
+
+order = ['F', 'A', 'I', 'R']
+if sys.argv:
+    try:
+        doi = sys.argv[1]
+    except:
+        doi = "http://dx.doi.org/10.17632/crnmszmb8h.1"
+        doi = "http://dx.doi.org/10.17632/yjrpmr5mwn.1"
+        #doi = "http://dx.doi.org/10.17632/nhtjgdkft4.1"
+
 data = fair_ranking(doi)
-print data
+print doi
+for c in order:
+    print "%s %s" % (c, data[c])
